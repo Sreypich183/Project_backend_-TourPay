16b9985f651251f6e099e12679e4324c0243596a
Kim-Solida <Solida.kim@student.cadt.edu.kh>
2025-07-31 17:18:45 +0700

    Delete frontend directory

--- frontend/src/components/FreeCard.jsx
+++
@@ -1,3 +0,0 @@
-export default function FreeCard() {
-  return <div>FreeCard Feature Coming Soon!</div>;
-}
--- frontend/src/components/Request.jsx
+++
@@ -1,8 +0,0 @@
-export default function Request() {
-  return (
-    <div>
-      <h2>Connect Your Bank Account</h2>
-      <button>Connect with Bank</button>
-    </div>
-  );
-}
